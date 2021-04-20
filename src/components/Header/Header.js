6fcdf0a90adf4a9c509242a3ee5c1c07f0bd90b2
Solovev Pavel <79838237+KettoKujira@users.noreply.github.com>
2021-04-20 11:49:47 +0700

    Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,10 @@ const Header = () => {
   return (
     <header className='header'>
       <h1 className='header_title'>Taskmanager</h1>
-      <button className='header_button'>New task +</button>
-      <span className='header_date'>Today is 19 april 2021</span>
+      <button className='header_button'><span className='button_text'>New task +</span></button>
+      <span className='header_date'>Today is
+      <span className='header_date--color'> 19 april 2021</span>
+      </span>
     </header>
   );
 };
