3b77aacc4fd07ddb0fb29ec8f97ea14b94190146
Solovev Pavel <79838237+KettoKujira@users.noreply.github.com>
2021-04-20 09:47:17 +0700

    Add layout to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header>
+        <h1>Taskmanager</h1>
+        <button>New task +</button>
+        <span>Today is 19 april 2021</span>
       </header>
+      <main>
+        <h2>What's up today?</h2>
+        <div>
+          <span>19 april 2021</span>
+          <h3>Learn to code</h3>
+          <p>Today i will learn how to manage store.</p>
+          <button>Delete</button>
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
