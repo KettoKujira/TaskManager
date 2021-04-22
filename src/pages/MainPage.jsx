ea610b64f9555ed59b2acc8648fca5be32720013
Artem Cherenkov <art.cherenkov@gmail.com>
2021-04-22 23:12:48 +0700

    feat: создать компонент TasksList и перенести стили блока tasks

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,77 +1,13 @@
 import Header from "../components/layout/Header/Header";
 import Main from "../components/layout/Main/Main";
+import TasksList from "../components/TasksList/TasksList";
 
 const MainPage = () => {
   return (
     <>
       <Header />
       <Main>
-        <ul className="main__tasks tasks">
-          <li className="tasks__item task">
-            <p className="task__date">19 april 2021</p>
-            <h3 className="task__title">Learn to code</h3>
-            <p className="task__content">
-              Today I will learn how to manage state.
-            </p>
-            <button className="task__delete-button button" />
-          </li>
-          <li className="tasks__item task">
-            <p className="task__date">19 april 2021</p>
-            <h3 className="task__title">Learn to code</h3>
-            <p className="task__content">
-              Today I will learn how to manage state.
-            </p>
-            <button className="task__delete-button button" />
-          </li>
-          <li className="tasks__item task">
-            <p className="task__date">19 april 2021</p>
-            <h3 className="task__title">Learn to code</h3>
-            <p className="task__content">
-              Today I will learn how to manage state.
-            </p>
-            <button className="task__delete-button button" />
-          </li>
-          <li className="tasks__item task">
-            <p className="task__date">19 april 2021</p>
-            <h3 className="task__title">Learn to code</h3>
-            <p className="task__content">
-              Today I will learn how to manage state.
-            </p>
-            <button className="task__delete-button button" />
-          </li>
-          <li className="tasks__item task">
-            <p className="task__date">19 april 2021</p>
-            <h3 className="task__title">Learn to code</h3>
-            <p className="task__content">
-              Today I will learn how to manage state.
-            </p>
-            <button className="task__delete-button button" />
-          </li>
-          <li className="tasks__item task">
-            <p className="task__date">19 april 2021</p>
-            <h3 className="task__title">Learn to code</h3>
-            <p className="task__content">
-              Today I will learn how to manage state.
-            </p>
-            <button className="task__delete-button button" />
-          </li>
-          <li className="tasks__item task">
-            <p className="task__date">19 april 2021</p>
-            <h3 className="task__title">Learn to code</h3>
-            <p className="task__content">
-              Today I will learn how to manage state.
-            </p>
-            <button className="task__delete-button button" />
-          </li>
-          <li className="tasks__item task">
-            <p className="task__date">19 april 2021</p>
-            <h3 className="task__title">Learn to code</h3>
-            <p className="task__content">
-              Today I will learn how to manage state.
-            </p>
-            <button className="task__delete-button button" />
-          </li>
-        </ul>
+        <TasksList />
       </Main>
     </>
   );
