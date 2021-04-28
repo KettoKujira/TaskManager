b181ddc0c6e4d193a2ea5c1f66b0d0e886d42aeb
Solovev Pavel <79838237+KettoKujira@users.noreply.github.com>
2021-04-28 13:58:25 +0700

    feat: добавить главный компонент App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import MainPage from "./pages/MainPage/MainPage";
+import App from "./components/App/App";
 import "./assets/styles/index.css";
 
 const TASKS = [
@@ -33,7 +33,7 @@ const TASKS = [
 
 ReactDOM.render(
   <React.StrictMode>
-    <MainPage tasks={TASKS} />
+    <App tasks={TASKS} />
   </React.StrictMode>,
   document.getElementById("root")
 );
