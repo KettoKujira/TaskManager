97d83b400ecb410f2ca6e37a76cb700d7cb2cc6b
Solovev Pavel <79838237+KettoKujira@users.noreply.github.com>
2021-04-28 13:17:11 +0700

    refactor: вынести проп task в отдельный файл

---
+++ src/props/task.prop.js
@@ -0,0 +1,8 @@
+import PropTypes from "prop-types";
+
+export default PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    date: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+  });
