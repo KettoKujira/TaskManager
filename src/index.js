ca9174fc82fdac397ec9dfb41430700b1b1c0114
Solovev Pavel <79838237+KettoKujira@users.noreply.github.com>
2021-05-08 10:13:35 +0700

    feat: добавить TaskDate

--- src/index.js
+++ src/index.js
@@ -7,25 +7,25 @@ import "./assets/styles/index.css";
 const TASKS = [
   {
     id: 1,
-    date: "22 april 2021",
+    date: new Date(2021, 3, 22),
     title: "Hello World",
     content: "Today is awesome day!",
   },
   {
     id: 2,
-    date: "21 april 2021",
+    date: new Date(2021, 3, 22),
     title: "Hi World",
     content: "Today is a great night!",
   },
   {
     id: 3,
-    date: "20 april 2021",
+    date: new Date(2021, 3, 22),
     title: "Bye World",
     content: "Today is big evening!",
   },
   {
     id: 4,
-    date: "19 april 2021",
+    date: new Date(2021, 3, 22),
     title: "Goodbye World",
     content: "Today is good morning!",
   },
--- src/props/task.prop.js
+++ src/props/task.prop.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 export default PropTypes.shape({
     id: PropTypes.number.isRequired,
-    date: PropTypes.string.isRequired,
+    date: PropTypes.object.isRequired,
     title: PropTypes.string.isRequired,
     content: PropTypes.string.isRequired,
   });
