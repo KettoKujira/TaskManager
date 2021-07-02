37188b17591bbfcce8032a4a65e9185463a2bf2d
Solovev Pavel <79838237+KettoKujira@users.noreply.github.com>
2021-07-02 17:00:31 +0700

    feat: Greeting away animation

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,25 @@
-import Greeting from '../Greeting/Greeting';
+import React, { useState, useEffect } from "react";
+
+import Greeting from "../Greeting/Greeting";
 import Header from "../layout/Header/Header";
 import Main from "../layout/Main/Main";
 
 const App = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const Visible = () => {
+    setIsVisible(false);
+  };
+
+  setTimeout(Visible, 4000);
+
   return (
     <>
-      <Greeting />
+      {isVisible ? <Greeting /> : ''}
       <Header />
       <Main />
     </>
-  );;
+  );
 };
 
 export default App;
